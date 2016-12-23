5827b27163a6f326fd677afb851faaebbfbcdbc8
wangqihui <wangqihui0324@gmail.com>
2016-12-23 17:33:28 +0800

    初步识别率在50左右

--- face_recognition.py
+++ face_recognition.py
@@ -5,240 +5,77 @@ import cv2
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import time
+import sys
+import os
+import MySQLdb
 
 
-"""
-# 简介
-# 图6-1中的矩阵
-img = np.array([
-    [[255, 0, 0], [0, 255, 0], [0, 0, 255]],
-    [[255, 255, 0], [255, 0, 255], [0, 255, 255]],
-    [[255, 255, 255], [128, 128, 128], [0, 0, 0]],
-], dtype=np.uint8)
+def face(imagePath):
+    # Get user supplied values
+    # imagePath = 'example/FaceDetect-master/abba.png'
+    imageName = 'result_'+imagePath.split('/')[-1]
+    cascPath = 'example/haarcascade_frontalface_default.xml'
 
-# 用matplotlib存储  RGB
-plt.imsave('img/img_pyplot.jpg', img)
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascPath)
 
-# 用OpenCV存储  BGR
-cv2.imwrite('img/img_cv2.jpg', img)
-"""
+    # Read the image
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-"""
-# 存取图像
-# 读取一张400x600分辨率的图像
-color_img = cv2.imread('img/test_240*240.jpg')
-print(color_img.shape)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
 
-# 直接读取单通道
-gray_img = cv2.imread('img/test_240*240.jpg', cv2.IMREAD_GRAYSCALE)
-print(gray_img.shape)
+    print("Found {0} faces in {1}".format(len(faces), imagePath))
 
-# 把单通道图片保存后，再读取，仍然是3通道，相当于把单通道值复制到3个通道保存
-cv2.imwrite('img/test_grayscale.jpg', gray_img)
-reload_grayscale = cv2.imread('img/test_grayscale.jpg')
-print(reload_grayscale.shape)
+    # Draw a rectangle around the faces
+    data_list = []
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        data_list.append([(x, y), (x+w, y+h)])
 
-# cv2.IMWRITE_JPEG_QUALITY指定jpg质量，范围0到100，默认95，越高画质越好，文件越大
-cv2.imwrite('img/test_imwrite_best.jpg', color_img, (cv2.IMWRITE_JPEG_QUALITY, 100))
+    cv2.imwrite('test_image_face/'+imageName, image)
 
-# cv2.IMWRITE_PNG_COMPRESSION指定png质量，范围0到9，默认3，越高文件越小，画质越差
-cv2.imwrite('img/test_imwrite_best.png', color_img, (cv2.IMWRITE_PNG_COMPRESSION, 0))
-"""
-
-"""
-# 缩放，裁剪和补边
-# 读取一张四川大录古藏寨的照片
-img = cv2.imread('img/test_240*240.jpg')
-
-# 缩放成200x200的方形图像
-img_200x200 = cv2.resize(img, (200, 200))
-
-# 不直接指定缩放后大小，通过fx和fy指定缩放比例，0.5则长宽都为原来一半
-# 等效于img_200x300 = cv2.resize(img, (300, 200))，注意指定大小的格式是(宽度,高度)
-# 插值方法默认是cv2.INTER_LINEAR，这里指定为最近邻插值
-img_120x120 = cv2.resize(img, (0, 0), fx=0.5, fy=0.5,
-                              interpolation=cv2.INTER_NEAREST)
-
-# 在上张图片的基础上，上下各贴50像素的黑边，生成300x300的图像
-img_240x340 = cv2.copyMakeBorder(img, 50, 50, 0, 0,
-                                       cv2.BORDER_CONSTANT,
-                                       value=(0, 0, 0))
-
-# 对照片中树的部分进行剪裁  原点在左上角，横轴为第一个元素！
-patch_tree = img[20:60, 0:180]
-
-cv2.imwrite('img/cropped_tree.jpg', patch_tree)
-cv2.imwrite('img/resized_200x200.jpg', img_200x200)
-cv2.imwrite('img/resized_200x300.jpg', img_120x120)
-cv2.imwrite('img/bordered_300x300.jpg', img_240x340)
-"""
+    return [imagePath, len(faces), str(data_list), 'test_image_face/'+imageName]
 
 
-"""
-# 色调，明暗，直方图和Gamma曲线
-# 通过cv2.cvtColor把图像从BGR转换到HSV
-img = cv2.imread('img/test_240*240.jpg')
-img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def insert(name, number_detect, number_detect_data, name_detect):
+    # 打开数据库连接
+    db = MySQLdb.connect("localhost", "root", "password", "face_recognition")
 
-# H空间中，绿色比黄色的值高一点，所以给每个像素+15，黄色的树叶就会变绿
-turn_green_hsv = img_hsv.copy()
-turn_green_hsv[:, :, 0] = (turn_green_hsv[:, :, 0]+15) % 180
-turn_green_img = cv2.cvtColor(turn_green_hsv, cv2.COLOR_HSV2BGR)
-cv2.imwrite('img/turn_green.jpg', turn_green_img)
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
 
-# 减小饱和度会让图像损失鲜艳，变得更灰
-colorless_hsv = img_hsv.copy()
-colorless_hsv[:, :, 1] = 0.5 * colorless_hsv[:, :, 1]
-colorless_img = cv2.cvtColor(colorless_hsv, cv2.COLOR_HSV2BGR)
-cv2.imwrite('img/colorless.jpg', colorless_img)
+    # SQL 插入语句
+    sql = "INSERT INTO face(name, \
+           number_detect, number_detect_data, name_detect) \
+           VALUES ('%s', '%d', '%s', '%s')" % \
+          (name, number_detect, number_detect_data, name_detect)
+    try:
+        # 执行sql语句
+        cursor.execute(sql)
+        # 提交到数据库执行
+        db.commit()
+    except:
+        # 发生错误时回滚
+        db.rollback()
 
-# 减小明度为原来一半
-darker_hsv = img_hsv.copy()
-darker_hsv[:, :, 2] = 0.5 * darker_hsv[:, :, 2]
-darker_img = cv2.cvtColor(darker_hsv, cv2.COLOR_HSV2BGR)
-cv2.imwrite('img/darker.jpg', darker_img)
-"""
-
-"""
-# 分通道计算每个通道的直方图
-img = cv2.imread('img/test_240*240.jpg')
-hist_b = cv2.calcHist([img], [0], None, [256], [0, 256])
-hist_g = cv2.calcHist([img], [1], None, [256], [0, 256])
-hist_r = cv2.calcHist([img], [2], None, [256], [0, 256])
+    # 关闭数据库连接
+    db.close()
 
 
-# 定义Gamma矫正的函数
-def gamma_trans(img, gamma):
-    # 具体做法是先归一化到1，然后gamma作为指数值求出新的像素值再还原
-    gamma_table = [np.power(x / 255.0, gamma) * 255.0 for x in range(256)]
-    gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
-
-    # 实现这个映射用的是OpenCV的查表函数
-    return cv2.LUT(img, gamma_table)
+def main():
+    file_list = os.listdir('./test_image')
+    for file in file_list:
+        (name, number_detect, number_detect_data, name_detect) = face('test_image/'+file)
+        insert(name, number_detect, number_detect_data, name_detect)
 
 
-# 执行Gamma矫正，小于1的值让暗部细节大量提升，同时亮部细节少量提升
-img_corrected = gamma_trans(img, 0.5)
-cv2.imwrite('img/gamma_corrected.jpg', img_corrected)
-
-# 分通道计算Gamma矫正后的直方图
-hist_b_corrected = cv2.calcHist([img_corrected], [0], None, [256], [0, 256])
-hist_g_corrected = cv2.calcHist([img_corrected], [1], None, [256], [0, 256])
-hist_r_corrected = cv2.calcHist([img_corrected], [2], None, [256], [0, 256])
-
-# 将直方图进行可视化
-
-fig = plt.figure()
-
-pix_hists = [
-    [hist_b, hist_g, hist_r],
-    [hist_b_corrected, hist_g_corrected, hist_r_corrected]
-]
-
-pix_vals = range(256)
-for sub_plt, pix_hist in zip([121, 122], pix_hists):
-    ax = fig.add_subplot(sub_plt, projection='3d')
-    for c, z, channel_hist in zip(['b', 'g', 'r'], [20, 10, 0], pix_hist):
-        cs = [c] * 256
-        ax.bar(pix_vals, channel_hist, zs=z, zdir='y', color=cs, alpha=0.618, edgecolor='none', lw=0)
-
-    ax.set_xlabel('Pixel Values')
-    ax.set_xlim([0, 256])
-    ax.set_ylabel('Counts')
-    ax.set_zlabel('Channels')
-
-plt.show()
-"""
-
-"""
-# 仿射变换
-# 读取一张斯里兰卡拍摄的大象照片
-img = cv2.imread('img/test_240*240.jpg')
-
-# 沿着横纵轴放大1.6倍，然后平移(-150,-240)，最后沿原图大小截取，等效于裁剪并放大
-M_crop_elephant = np.array([
-    [1.6, 0, -100],
-    [0, 1.6, -200]
-], dtype=np.float32)
-
-img_elephant = cv2.warpAffine(img, M_crop_elephant, (240, 240))
-cv2.imwrite('img/lanka_elephant.jpg', img_elephant)
-
-# x轴的剪切变换，角度15°
-theta = 15 * np.pi / 180
-M_shear = np.array([
-    [1, np.tan(theta), 0],
-    [0, 1, 0]
-], dtype=np.float32)
-
-img_sheared = cv2.warpAffine(img, M_shear, (240, 240))
-cv2.imwrite('img/lanka_safari_sheared.jpg', img_sheared)
-
-# 顺时针旋转，角度15°
-M_rotate = np.array([
-    [np.cos(theta), -np.sin(theta), 0],
-    [np.sin(theta), np.cos(theta), 0]
-], dtype=np.float32)
-
-img_rotated = cv2.warpAffine(img, M_rotate, (240, 240))
-cv2.imwrite('img/lanka_safari_rotated.jpg', img_rotated)
-
-# 某种变换，具体旋转+缩放+旋转组合可以通过SVD分解理解
-M = np.array([
-    [1, 1.5, -200],
-    [0.5, 2, -50]
-], dtype=np.float32)
-
-img_transformed = cv2.warpAffine(img, M, (240, 240))
-cv2.imwrite('img/lanka_safari_transformed.jpg', img_transformed)
-"""
-
-
-"""
-# 跳过
-# 视频功能
-
-interval = 60  # 捕获图像的间隔，单位：秒
-num_frames = 500  # 捕获图像的总帧数
-out_fps = 24  # 输出文件的帧率
-
-# VideoCapture(0)表示打开默认的相机
-cap = cv2.VideoCapture(0)
-
-# 获取捕获的分辨率
-size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
-        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-
-# 设置要保存视频的编码，分辨率和帧率
-video = cv2.VideoWriter(
-    "time_lapse.avi",
-    cv2.VideoWriter_fourcc('M', 'P', '4', '2'),
-    out_fps,
-    size
-)
-
-# 对于一些低画质的摄像头，前面的帧可能不稳定，略过
-for i in range(42):
-    cap.read()
-
-# 开始捕获，通过read()函数获取捕获的帧
-try:
-    for i in range(num_frames):
-        _, frame = cap.read()
-        video.write(frame)
-
-        # 如果希望把每一帧也存成文件，比如制作GIF，则取消下面的注释
-        # filename = '{:0>6d}.png'.format(i)
-        # cv2.imwrite(filename, frame)
-
-        print('Frame {} is captured.'.format(i))
-        time.sleep(interval)
-except KeyboardInterrupt:
-    # 提前停止捕获
-    print('Stopped! {}/{} frames captured!'.format(i, num_frames))
-
-# 释放资源并写入视频文件
-video.release()
-cap.release()
-"""
-
+if __name__ == '__main__':
+    main()
